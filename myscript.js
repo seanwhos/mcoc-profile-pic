af0cf1272e08321b5931dbe1b126b0a7fb3ac9b1
Sean Hughes <seanjhughes92@gmail.com>
2020-06-29 17:38:19 +0100

    changed char limit and font size

--- myscript.js
+++ myscript.js
@@ -9,11 +9,11 @@ const avatarImage = document.getElementsByClassName('user-picture');
 nameButton.addEventListener('click', function() {
     let userInput = prompt('Enter character name:', `Max length about 16 characters`);
         
-        if (userInput.length > 12) {
+        if (userInput.length > 11) {
             avatarName[0].style.fontSize = "17pt";
-            avatarName[0].style.padding = "5px 0 5px 0";
+            avatarName[0].style.padding = "5px 0 6px 0";
             avatarName[1].style.fontSize = "17pt";
-            avatarName[1].style.padding = "5px 0 5px 0";
+            avatarName[1].style.padding = "5px 0 6px 0";
         } else {
             avatarName[0].style.fontSize = "";
             avatarName[0].style.padding = "";
